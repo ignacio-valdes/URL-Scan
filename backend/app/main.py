a7333761c06d79f842c54d2471a707e3453612fc
Ignacio Valdés <ignacio.valdes.espinoza@outlook.com>
2025-10-22 18:24:16 -0300

    fix(api): add return value and fix cors methods

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,25 @@
 from app.services.virustotal import get_url
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import requests
 
+origins = ["*"]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 url = "https://www.google.com/"
 
-app.post("/Analizar_URL")
+app.post("/analizar_URL")
 def analizar_url(data: dict):
     url = data.get("url")
     print("Analizando la URL:", url)
-    get_url(url)
+    reporte = get_url(url)
+    return reporte
     
-
-analizar_url({"url": url})
